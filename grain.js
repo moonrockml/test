b537fb9b0709d9eefd5f2097a1605ef8661dfc17
moonrockml <141372970+moonrockml@users.noreply.github.com>
2024-03-17 22:55:14 -0400

    Add files via upload

---
+++ grain.js
@@ -0,0 +1,11 @@
+var options = {
+    animate: true,
+    patternWidth: 500,
+    patternHeight: 392.62,
+    grainOpacity: 0.6,
+    grainDensity: 1,
+    grainWidth: 1,
+    grainHeight: 1,
+  };
+
+  grained("#grainy-background", options);
